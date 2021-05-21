d4be6e2454b927e606d24aa4a045edbcaabbf3f0
Nikita Efremov <nikita-efremov-00@mail.ru>
2021-05-21 12:55:41 +0400

    Diplom v.1.0

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -4,15 +4,19 @@
  * В первую очередь это открытие или
  * закрытие имеющихся окон
  * */
-class Modal {
+ class Modal {
   /**
    * Устанавливает текущий элемент в свойство element
    * Регистрирует обработчики событий с помощью Modal.registerEvents()
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor(element){
-
+  constructor(element) {
+      if (!element) {
+          throw new Error('Элемент не существует');
+      }
+      this.element = element;
+      this.registerEvents();
   }
 
   /**
@@ -21,7 +25,10 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+      const buttons = this.element.querySelectorAll('[data-dismiss="modal"]');
+      [...buttons].forEach((button) => {
+          button.addEventListener('click', (e) => this.onClose(e));
+      });
   }
 
   /**
@@ -29,19 +36,20 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+      e.preventDefault();
+      this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+      this.element.style.display = 'block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
-  close(){
-
+  close() {
+      this.element.style.display = '';
   }
 }
